a11d7cac8dcf13d18e3b7245fb86131b15595362
gungun88 <pengqibao3@gmail.com>
2026-09-17 04:14:49 +0800

    Fix raffle ticket admin registration for Flarum 2

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,1 +1,27 @@
-export { default as extend } from './extend';
+import app from 'flarum/admin/app';
+
+app.initializers.add('lowseekai/flarum-raffle-ticket', () => {
+  const registry = app.registry.for('lowseekai-raffle-ticket');
+
+  registry
+    .registerSetting({
+      setting: 'ziven-guaguale.guagualeDisplayName',
+      type: 'text',
+      label: app.translator.trans('ziven-guaguale.admin.settings.guaguale-display-name'),
+      help: app.translator.trans('ziven-guaguale.admin.settings.guaguale-display-name-default'),
+    })
+    .registerSetting({
+      setting: 'ziven-guaguale.guagualeTimezone',
+      type: 'text',
+      label: app.translator.trans('ziven-guaguale.admin.settings.guaguale-timezone'),
+      help: app.translator.trans('ziven-guaguale.admin.settings.guaguale-timezone-help'),
+    })
+    .registerPermission(
+      {
+        icon: 'fas fa-ticket-alt',
+        label: app.translator.trans('ziven-guaguale.admin.guaguale-allow-guaguale'),
+        permission: 'ziven.zivenAllowGuaGuaLe',
+      },
+      'moderate'
+    );
+});
